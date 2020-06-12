97d880802927d4cba932f082c00f25afa3d99431
sdadn <sdadn@git.com>
2020-06-12 10:39:42 -0500

    Update: added more diagrams

--- docs/uml/build.sh
+++ docs/uml/build.sh
@@ -1,1 +1,5 @@
-./node_modules/.bin/mmdc -i docs/uml/linegraph.mmd -o docs/uml/uml_output/linegraph.png
+./node_modules/.bin/mmdc -i docs/uml/construct.mmd -o docs/uml/uml_output/construct.svg -w 1024 -H 768
+./node_modules/.bin/mmdc -i docs/uml/graphcontent.mmd -o docs/uml/uml_output/graphcontent.svg -w 1024 -H 768
+./node_modules/.bin/mmdc -i docs/uml/controls.mmd -o docs/uml/uml_output/controls.svg -w 1024 -H 768
+./node_modules/.bin/mmdc -i docs/uml/linegraph.mmd -o docs/uml/uml_output/linegraph.svg -w 1024 -H 768
+
