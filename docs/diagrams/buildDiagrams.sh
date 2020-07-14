28c5bad863402f33964d417e58aa3c424430f63c
sdadn <sdadn@git.com>
2020-07-14 16:10:00 -0500

    added diagrams

--- docs/diagrams/buildDiagrams.sh
+++ docs/diagrams/buildDiagrams.sh
@@ -1,4 +1,4 @@
   # ./node_modules/.bin/mmdc -i docs/diagrams/axis/ticksGeneration.mmd -o docs/diagrams/axis/ticksGeneration.png -w 1024 -H 768
 java -jar docs/diagrams/plantuml.jar docs/diagrams
 java -jar docs/diagrams/plantuml.jar docs/diagrams/line
-
+java -jar docs/diagrams/plantuml.jar docs/diagrams/graph
