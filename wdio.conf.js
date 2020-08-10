ca984dd3e9f0f093c7ece0b21c5514399cb15196
AshishMotanamGurunadham <motanamashish@gmail.com>
2020-08-09 23:10:56 -0500

    Update: made changes to travis.yml file test-10

--- wdio.conf.js
+++ wdio.conf.js
@@ -8,23 +8,23 @@ const config = {
         // Disable if running in CI pipeline
         enabled: !process.env.CI,
     },*/
-    capabilities : [/*{
+    capabilities : [{
         browserName: 'chrome',
         maxInstances: 1,
         loggingPrefs: {
             performance: 'ALL',
         },
         'goog:chromeOptions': {
-            /!** Run in headless mode since Chrome 69 cannot reach the tiny viewport size due to a omnibox size changexx
+            /** Run in headless mode since Chrome 69 cannot reach the tiny viewport size due to a omnibox size changexx
              * made by the chrome team. See https://bugs.chromium.org/p/chromedriver/issues/detail?id=2626#c1 &&
              * https://bugs.chromium.org/p/chromium/issues/detail?id=849784.
-             *!/
+             */
             args: ['headless', 'disable-gpu'],
             perfLoggingPrefs: {
                 traceCategories: 'blink.console, devtools.timeline, toplevel, disabled-by-default-devtools.timeline, disabled-by-default-devtools.timeline.frame',
             },
         },
-    },*/
+    },
         {
             browserName: 'firefox',
             maxInstances: 1,
